491e479885101e224de7558c67e07d9236829bf1
Albert <albert@qbids.nl>
2014-06-08 08:25:02 +0200

    Changed legend function so that the line shows correctly

--- plot3.R
+++ plot3.R
@@ -26,7 +26,7 @@ plot(data$DateTime,data$Sub_metering_1, type="n", xlab="", ylab="Energy sub mete
 lines(data$DateTime,data$Sub_metering_1, type="l", col="black")
 lines(data$DateTime,data$Sub_metering_2, type="l", col="red")
 lines(data$DateTime,data$Sub_metering_3, type="l", col="blue")
-legend("topright", pch="______", col=c("black", "blue", "red"), legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+legend("topright", lty=c(1,1), col=c("black", "blue", "red"), legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
 
 #create a .png file
 dev.copy(png, file="plot3.png", height=480, width=480)
--- plot4.R
+++ plot4.R
@@ -38,7 +38,7 @@ lines(data$DateTime,data$Sub_metering_2, type="l", col="red")
 lines(data$DateTime,data$Sub_metering_3, type="l", col="blue")
 #make the legendbox a bit smaller: cex=0.75
 #and no line around the legend (boxtype): bty="n"
-legend("topright", pch="______", col=c("black", "blue", "red"), legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), cex=0.75,bty="n")
+legend("topright", lty=c(1,1), col=c("black", "blue", "red"), legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), cex=0.75,bty="n")
 
 #fourth plot, lower right
 plot(data$DateTime,data$Global_reactive_power, type="l", xlab="datetime", ylab="Global_reactive_power")
